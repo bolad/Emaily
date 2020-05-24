46cca438f8d99271a5d036073e74a5a0c071731a
stanley akyea <stanley.akyea@gmail.com>
2020-05-24 19:55:04 +0200

    Add authRoutes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
 //get access to the expres library
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
+
+require('./models/user')
+
+//import the passport.js file from services
+require('./services/passport');
+
+const app = express();
+
+app.use(
+  cookieSession({
+    //convert 30days to milliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+//tell pasport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+//take the app object and attach the auth routes to it
+require('./routes/authRoutes')(app);
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -11,14 +35,6 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: tru
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
-require('./models/user')
-
-//import the passport.js file from services
-require('./services/passport');
-
-//take the app object and attach the auth routes to it
-require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,22 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users')
 
+//generate identifying piece of info(cookie) for the user
+//turn user model instance into an id
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+  //NB: user.id !== profile.id
+});
+
+//take the identifying piece of user info(id) and
+//turn id into user model instance 
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+});
+
 passport.use(
   new GoogleStrategy(
     {
